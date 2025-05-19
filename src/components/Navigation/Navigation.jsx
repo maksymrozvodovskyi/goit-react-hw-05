4651329f4c3c4ee364ea8888d3faa52fa4500f97
maksymrozvodovskyi <maksym.rozvodovskyi@ukr.net>
2025-05-19 23:19:27 +0300

    save commit

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -2,20 +2,17 @@ import { NavLink } from "react-router-dom";
 import css from "./Navigation.module.css";
 import clsx from "clsx";
 
+const setActiveLink = ({ isActive }) =>
+  clsx(css.link, isActive && css.isActive);
+
 export default function Navigation() {
   return (
     <header className={css.header}>
       <nav className={css.nav}>
-        <NavLink
-          to="/"
-          className={({ isActive }) => clsx(css.link, isActive && css.isActive)}
-        >
+        <NavLink to="/" className={setActiveLink}>
           Home
         </NavLink>
-        <NavLink
-          to="/movies"
-          className={({ isActive }) => clsx(css.link, isActive && css.isActive)}
-        >
+        <NavLink to="/movies" className={setActiveLink}>
           Movies
         </NavLink>
       </nav>
