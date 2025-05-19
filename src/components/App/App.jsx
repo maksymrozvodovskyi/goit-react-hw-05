cd3c6e0bc2a5bb94ecaa9e33cbe813b5ea0eb8d5
maksymrozvodovskyi <maksym.rozvodovskyi@ukr.net>
2025-05-19 18:00:08 +0300

    save commit

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,19 @@
 import css from "./App.module.css";
+import { Routes } from "react-router-dom";
+
+import Navigation from "../Navigation/Navigation";
 
 export default function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Navigation />
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/movies" element={<MoviesPage />} />
+        {/* <Route path="/movies/:movieId" element={<MovieDetailsPage />} /> */}
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </div>
+  );
 }
---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,17 @@
+import { NavLink } from "react-router-dom";
+import css from "./Navigation.module.css";
+
+export default function Navigation() {
+  return (
+    <header className={css.header}>
+      <nav className={css.nav}>
+        <NavLink to="/" className={css.link}>
+          Home
+        </NavLink>
+        <NavLink to="/movies" className={css.link}>
+          Movies
+        </NavLink>
+      </nav>
+    </header>
+  );
+}
