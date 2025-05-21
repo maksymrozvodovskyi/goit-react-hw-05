173ecd6db09ed34bec9a3040c66da81793594ce5
maksymrozvodovskyi <maksym.rozvodovskyi@ukr.net>
2025-05-21 16:19:03 +0300

    save commit

--- src/components/MovieCast/MovieCast.jsx
+++ src/components/MovieCast/MovieCast.jsx
@@ -21,7 +21,11 @@ export default function MovieCast() {
           {cast.map((actor) => (
             <li key={actor.id} className={css.castItem}>
               <img
-                src={`https://image.tmdb.org/t/p/w500${actor.profile_path}`}
+                src={
+                  actor.profile_path
+                    ? `https://image.tmdb.org/t/p/w500${actor.profile_path}`
+                    : "https://dl-media.viber.com/10/share/2/long/vibes/icon/image/0x0/95e0/5688fdffb84ff8bed4240bcf3ec5ac81ce591d9fa9558a3a968c630eaba195e0.jpg"
+                }
                 alt={actor.name}
                 className={css.actorImage}
               />
